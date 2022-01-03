e2d0dacd45c59a42435412e642eee9f332c9bb58
Bryan Appleby <bryanjappleby@gmail.com>
2022-01-03 14:31:20 +0200

    Add execution time to letter solver

--- letters/letters.py
+++ letters/letters.py
@@ -1,7 +1,8 @@
 from lettersHelper import *
-import sys
+import sys, time
 
 if __name__ == "__main__":
+    start_time = time.time()
     pool = sys.argv[1]
     words = findAllFromDefaultDictionary(pool)
     if len(words) == 0:
@@ -9,3 +10,4 @@ if __name__ == "__main__":
     else:
         best = max(words, key=len)
         print("%s (%s)"%(best, len(best)))
+    print("Completed in %.2f seconds" % (time.time() - start_time))
